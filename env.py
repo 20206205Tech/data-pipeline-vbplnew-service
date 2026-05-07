56141ece4b455b307c85bc89f359aee7a62c45ac
vuvannghiawork <vuvannghia.work@gmail.com>
2026-05-07 23:32:33 +0700

    1

--- env.py
+++ env.py
@@ -1,3 +1,5 @@
+import os
+
 from environs import Env
 from loguru import logger
 
@@ -5,14 +7,16 @@ env = Env()
 logger.info("Loading environment variables...")
 
 
-DATA_PIPELINE_VBPLNEW_DATABASE_URL = env.str("DATA_PIPELINE_VBPLNEW_DATABASE_URL")
-DATABASE_URL = DATA_PIPELINE_VBPLNEW_DATABASE_URL
+ENVIRONMENT = env.str("ENVIRONMENT", "production")
+RELOAD = True if ENVIRONMENT == "development" else False
+
+
 SERVICE_NAME = "data-pipeline-vbplnew-service"
 PORT = env.int("SERVICE_PORT", 51002)
 
 
-ENVIRONMENT = env.str("ENVIRONMENT", "production")
-RELOAD = True if ENVIRONMENT == "development" else False
+DATA_PIPELINE_VBPLNEW_DATABASE_URL = env.str("DATA_PIPELINE_VBPLNEW_DATABASE_URL")
+DATABASE_URL = DATA_PIPELINE_VBPLNEW_DATABASE_URL
 
 
 SUPABASE_PROJECT_ID = env.str("SUPABASE_PROJECT_ID")
@@ -25,10 +29,25 @@ AUDIENCE = "authenticated"
 DESCRIPTION = f"""
 # Chào mừng đến với {SERVICE_NAME} ({ENVIRONMENT})
 
+* [Google](https://{SUPABASE_PROJECT_ID}.supabase.co/auth/v1/authorize?provider=google)
+* [Database](https://console.neon.tech/app/org-still-feather-82034197/projects?q={SERVICE_NAME})
 * [Local](http://localhost:{PORT})
-* [Dev](https://dev-code-{SERVICE_NAME}.20206205.tech)
-* [Đăng nhập với Google](https://{SUPABASE_PROJECT_ID}.supabase.co/auth/v1/authorize?provider=google)
+* [Dev](https://dev-{SERVICE_NAME}.20206205.tech)
 
 """.strip()
 
+
 logger.success(f"DESCRIPTION: \n{DESCRIPTION}")
+
+
+PATH_FILE_ENV = os.path.abspath(__file__)
+PATH_FOLDER_PROJECT = os.path.dirname(PATH_FILE_ENV)
+PATH_FOLDER_DATA = os.path.join(PATH_FOLDER_PROJECT, "data")
+PATH_FOLDER_DOCS = os.path.join(PATH_FOLDER_PROJECT, "docs")
+
+
+if not os.path.exists(PATH_FOLDER_DATA):
+    os.makedirs(PATH_FOLDER_DATA)
+
+if not os.path.exists(PATH_FOLDER_DOCS):
+    os.makedirs(PATH_FOLDER_DOCS)

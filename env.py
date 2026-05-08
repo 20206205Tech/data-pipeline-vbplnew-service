76ec473167a89b1d8f791b816ec22b08524188fd
vuvannghiawork <vuvannghia.work@gmail.com>
2026-05-08 08:45:11 +0700

    1

--- env.py
+++ env.py
@@ -1,5 +1,3 @@
-import os
-
 from environs import Env
 from loguru import logger
 
@@ -38,16 +36,3 @@ DESCRIPTION = f"""
 
 
 logger.success(f"DESCRIPTION: \n{DESCRIPTION}")
-
-
-PATH_FILE_ENV = os.path.abspath(__file__)
-PATH_FOLDER_PROJECT = os.path.dirname(PATH_FILE_ENV)
-PATH_FOLDER_DATA = os.path.join(PATH_FOLDER_PROJECT, "data")
-PATH_FOLDER_DOCS = os.path.join(PATH_FOLDER_PROJECT, "docs")
-
-
-if not os.path.exists(PATH_FOLDER_DATA):
-    os.makedirs(PATH_FOLDER_DATA)
-
-if not os.path.exists(PATH_FOLDER_DOCS):
-    os.makedirs(PATH_FOLDER_DOCS)
